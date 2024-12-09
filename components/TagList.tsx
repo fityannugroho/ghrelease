889bc2b0b263c42acec5b92bc2fe3affaaccca32
Fityan <63894003+fityannugroho@users.noreply.github.com>
2024-12-09 23:25:03 +0700

    fix: better error handling

--- components/TagList.tsx
+++ components/TagList.tsx
@@ -68,59 +68,61 @@ export default function TagList({
     <div className="border p-4 rounded">
       <p className="text-lg font-semibold mb-4">Tags ({tags.length})</p>
 
-      {tagsQuery.status === 'pending' && <p>Loading tags...</p>}
+      {/* Filter tags */}
+      <Input
+        type="text"
+        placeholder="Filter tags"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value.toLowerCase())}
+        className="w-full p-2 border rounded my-4"
+      />
 
-      {tagsQuery.status === 'error' && (
-        <p className="text-destructive">{tagsQuery.error.message}</p>
-      )}
+      <ScrollArea className="mt-2 max-h-[10rem] lg:max-h-[16rem] overflow-y-auto">
+        <ul className="space-y-2">
+          {filteredTags.map((tag) => (
+            <li key={tag.name}>
+              <Button
+                variant={tag.name === selectedTag?.name ? 'default' : 'outline'}
+                onClick={() => {
+                  onTagSelect?.(tag)
+                }}
+                className="w-full truncate"
+              >
+                {tag.name}
+              </Button>
+            </li>
+          ))}
 
-      {tagsQuery.status === 'success' && (
-        <>
-          {/* Filter tags */}
-          <Input
-            type="text"
-            placeholder="Filter tags"
-            value={filter}
-            onChange={(e) => setFilter(e.target.value.toLowerCase())}
-            className="w-full p-2 border rounded my-4"
-          />
-
-          <ScrollArea className="mt-2 max-h-[10rem] lg:max-h-[16rem] overflow-y-auto">
-            <ul className="space-y-2">
-              {filteredTags.map((tag) => (
-                <li key={tag.name}>
-                  <Button
-                    variant={
-                      tag.name === selectedTag?.name ? 'default' : 'outline'
-                    }
-                    onClick={() => {
-                      onTagSelect?.(tag)
-                    }}
-                    className="w-full truncate"
-                  >
-                    {tag.name}
-                  </Button>
-                </li>
-              ))}
-              {tagsQuery.hasNextPage && (
-                <div ref={observerTarget} className="h-6">
-                  {tagsQuery.isFetchingNextPage && (
-                    <LoaderIcon className="animate-spin h-6 w-6 mx-auto">
-                      <span className="sr-only">Loading...</span>
-                    </LoaderIcon>
-                  )}
-                </div>
+          {/* Load more */}
+          {tagsQuery.hasNextPage && (
+            <div ref={observerTarget} className="h-6">
+              {tagsQuery.isFetchingNextPage && (
+                <LoaderIcon className="animate-spin h-6 w-6 mx-auto">
+                  <span className="sr-only">Loading...</span>
+                </LoaderIcon>
               )}
-            </ul>
+            </div>
+          )}
+        </ul>
+      </ScrollArea>
 
-            {!tagsQuery.hasNextPage && filteredTags.length === 0 && (
-              <p className="text-center text-gray-500">
-                No tags found. Try a different filter.
-              </p>
-            )}
-          </ScrollArea>
-        </>
+      {tagsQuery.isPending && (
+        <p className="text-center text-sm mt-2">Loading tags...</p>
       )}
+
+      {tagsQuery.isError && (
+        <p className="text-destructive text-center text-sm mt-2">
+          {tagsQuery.error.message}
+        </p>
+      )}
+
+      {tagsQuery.isSuccess &&
+        !tagsQuery.hasNextPage &&
+        filteredTags.length < 1 && (
+          <p className="text-center text-gray-500 dark:text-gray-400 text-sm mt-2">
+            No tags found. Try a different filter.
+          </p>
+        )}
     </div>
   )
 }
