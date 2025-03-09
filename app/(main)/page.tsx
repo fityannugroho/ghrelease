7473f1b4c9a1fb02fa818518804bb0ba5cb3cd1e
Fityan <63894003+fityannugroho@users.noreply.github.com>
2025-03-09 17:50:40 +0700

    feat: improve layout and styling on home and search components

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,14 +2,16 @@ import SearchRepos from '@/components/SearchRepos'
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl md:text-4xl font-bold my-4 lg:my-6 text-center">
-        GitHub Release Viewer
-      </h1>
-      <p className="text-center text-sm md:text-base mb-6 text-pretty max-w-lg mx-auto">
-        GitHub Release page with <b>better UX</b>. Read and discover GitHub
-        repository release notes with ease.
-      </p>
+    <div className="container mx-auto px-4">
+      <div className="my-10 lg:my-14">
+        <h1 className="text-2xl md:text-4xl mb-4 font-bold text-center">
+          GitHub Release Viewer
+        </h1>
+        <p className="text-center text-sm md:text-base text-pretty mb-2 max-w-lg mx-auto">
+          GitHub Release page with <b>better UX</b>. Read and discover GitHub
+          release notes with ease.
+        </p>
+      </div>
       <SearchRepos />
     </div>
   )
