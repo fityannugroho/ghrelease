05c37b2de59a56c1911eecbfc113dcbc1f92d6c0
Fityan <63894003+fityannugroho@users.noreply.github.com>
2025-03-10 13:41:30 +0700

    feat: add `Footer` component and integrate it into `MainLayout` (#14)

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,13 @@
+import { Footer } from '@/components/Footer'
 import { Navbar } from '@/components/Navbar'
 import type { PropsWithChildren } from 'react'
 
 export default function MainLayout({ children }: PropsWithChildren) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main>{children}</main>
-    </>
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
   )
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { CoffeeIcon } from 'lucide-react'
+import Link from 'next/link'
+
+export function Footer() {
+  const year = new Date().getFullYear()
+
+  return (
+    <footer className="border-t border-border/40 bg-background/95 py-3 mt-auto">
+      <div className="container mx-auto px-4">
+        <div className="text-center text-xs md:text-sm text-foreground/70">
+          {/* Copyright and info */}
+          <p className="mt-1">
+            &copy; {year}, built with{' '}
+            <span role="img" aria-label="coffee">
+              <CoffeeIcon className="inline mx-0.5 h-4 w-4" />
+            </span>{' '}
+            by{' '}
+            <Link
+              href="https://github.com/fityannugroho"
+              target="_blank"
+              className="underline"
+            >
+              fityannugroho
+            </Link>
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
